21a473887657ee2b59ea308f8200c19c31b3ea0d
JimvandeVen <jimvandeven_1@hotmail.com>
2019-04-18 14:51:06 +0200

    added the positive and negative tweet also added the feed of the first 10 tweets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const googleProfanityWords = require('google-profanity-words');
+// const googleProfanityWords = require('google-profanity-words');
 const Twit = require('twit')
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
@@ -30,71 +30,121 @@ function index(req, res) {
 let stream = T.stream('statuses/filter', { track: '#brexit', language: 'en', tweet_mode: "extended" })
 
 stream.on('tweet', function (stream) {
-
     streamHandler(stream)
-
 })
 
+let tweets = []
+let count = 0;
+
 function streamHandler(stream) {
+    const pattern = "RT @";
+    if (stream.in_reply_to_status_id === null && stream.text.indexOf(pattern) < 0 && stream.quoted_status_id === undefined) {
+        count++;
 
-    if (stream.extended_tweet) {
-        console.log(stream.extended_tweet.full_text)
-    } else {
-        console.log(stream.text)
+
+        checkFunc(stream)
+
     }
-
-    // let text = stream.extended_tweet ? stream.extended_tweet.full_text : stream.text
-    // console.log(text);
-
-
-    // let tweet = {
-    //     twid: stream.id,
-    //     active: false,
-    //     author: stream.user.name,
-    //     avatar: stream.user.profile_image_url,
-    //     body: text,
-    //     date: stream.created_at,
-    //     screenname: stream.user.screen_name,
-    //     sentiment: sentiment.analyze(text)
-    // };
-
-    // io.emit('tweet', tweet);
 }
 
-let badWords = googleProfanityWords.list()
+function checkFunc(stream) {
+    if (stream.extended_tweet) {
+        count++;
+        extendedTweet(stream)
+    } else {
+        tweet(stream)
+        count++;
+    }
+    autoFeed(tweets)
 
-io.on('connection', function (socket) {
+}
 
-    socket.broadcast.emit('newUser', socket.id);
+function autoFeed(tweets) {
 
-    socket.on('setUserName', function (nickname) {
-        socket.username = nickname;
-        console.log(socket.username)
-        io.emit('newName', nickname);
-    });
+    if (count < 10) {
+        console.log(tweets)
+        io.emit("autoFeed", tweets)
 
-    socket.on('chat message', function (msg) {
-        msg.username = socket.username
+    }
+}
 
-        let words = msg.message.split(" ")
-        let sensoredWords = []
-        words.forEach(word => {
-            if (badWords.includes(word)) {
-                let censoredWord = word.replace(/./g, "*")
-                sensoredWords.push(censoredWord)
-            } else {
-                sensoredWords.push(word)
-            }
-        });
-        let newMsg = sensoredWords.join(" ")
-        msg.message = newMsg
-        io.emit('chat message', msg);
-    });
+async function extendedTweet(stream) {
+    function crObj(stream) {
+        let tweet = {
+            twid: stream.id,
+            active: false,
+            author: stream.user.name,
+            avatar: stream.user.profile_image_url,
+            body: stream.extended_tweet.full_text,
+            date: stream.created_at,
+            screenname: stream.user.screen_name,
+            sentiment: sentiment.analyze(stream.extended_tweet.full_text)
+        };
+        return tweet
+    }
+    await tweets.push(crObj(stream))
+    io.emit('tweet', tweets.sort(compare));
+    console.log(tweets)
+}
 
-    socket.on('disconnect', function () {
-        console.log("disconect")
-        io.sockets.emit('userLeft', socket.username);
-    });
-});
+async function tweet(stream) {
+    function crObj(stream) {
+        let tweet = {
+            twid: stream.id,
+            active: false,
+            author: stream.user.name,
+            avatar: stream.user.profile_image_url,
+            body: stream.text,
+            date: stream.created_at,
+            screenname: stream.user.screen_name,
+            sentiment: sentiment.analyze(stream.text)
+        };
+        return tweet
+    }
+    await tweets.push(crObj(stream))
+    io.emit('tweet', tweets.sort(compare));
+    console.log(tweets)
+
+}
+
+function compare(a, b) {
+    return a.sentiment.score - b.sentiment.score
+}
+
+// let badWords = googleProfanityWords.list()
+
+// io.on('connection', function (socket) {
+
+//     socket.broadcast.emit('newUser', socket.id);
+
+//     socket.on('setUserName', function (nickname) {
+//         socket.username = nickname;
+//         console.log(socket.username)
+//         io.emit('newName', nickname);
+//     });
+
+//     socket.on('chat message', function (msg) {
+//         msg.username = socket.username
+
+//         let words = msg.message.split(" ")
+//         let sensoredWords = []
+//         words.forEach(word => {
+//             if (badWords.includes(word)) {
+//                 let censoredWord = word.replace(/./g, "*")
+//                 sensoredWords.push(censoredWord)
+//             } else {
+//                 sensoredWords.push(word)
+//             }
+//         });
+//         let newMsg = sensoredWords.join(" ")
+//         msg.message = newMsg
+//         io.emit('chat message', msg);
+//     });
+
+//     socket.on('disconnect', function () {
+//         console.log("disconect")
+//         io.sockets.emit('userLeft', socket.username);
+//     });
+// });
 
 http.listen(process.env.PORT || 3000)
--- public/main.js
+++ public/main.js
@@ -1,43 +1,62 @@
 const socket = io();
-const form = document.querySelector("#form");
-const message = document.querySelector("#m");
-const nameForm = document.querySelector("#nameForm");
+// const form = document.querySelector("#form");
+// const message = document.querySelector("#m");
+// const nameForm = document.querySelector("#nameForm");
 
-if (form) {
-    form.addEventListener("submit", e => {
-        e.preventDefault();
-        socket.emit("chat message", {
-            message: m.value
-        });
-        m.value = "";
-        return false;
-    });
-}
+// if (form) {
+//     form.addEventListener("submit", e => {
+//         e.preventDefault();
+//         socket.emit("chat message", {
+//             message: m.value
+//         });
+//         m.value = "";
+//         return false;
+//     });
+// }
 
-if (nameForm) {
-    nameForm.addEventListener("submit", e => {
-        e.preventDefault();
-        socket.emit("setUserName", nickname.value);
-        nickname.value = "";
-        nameForm.remove()
-        return false;
-    });
-}
+// if (nameForm) {
+//     nameForm.addEventListener("submit", e => {
+//         e.preventDefault();
+//         socket.emit("setUserName", nickname.value);
+//         nickname.value = "";
+//         nameForm.remove()
+//         return false;
+//     });
+// }
 
-socket.on('newName', function (nickname) {
-    console.log("new username for user: " + nickname);
+// socket.on('newName', function (nickname) {
+//     console.log("new username for user: " + nickname);
 
-    document.querySelector("#messages").innerHTML += "<li>" + nickname.toUpperCase() + " just joined the chatroom" + " </li>";
-});
+//     document.querySelector("#messages").innerHTML += "<li>" + nickname.toUpperCase() + " just joined the chatroom" + " </li>";
+// });
 
-socket.on("chat message", function (msg) {
-    console.log(msg);
+// socket.on("chat message", function (msg) {
+//     console.log(msg);
 
-    document.querySelector("#messages").innerHTML += "<li>" + msg.username.toUpperCase() + ":" + msg.message + " </li>";
-});
+//     document.querySelector("#messages").innerHTML += "<li>" + msg.username.toUpperCase() + ":" + msg.message + " </li>";
+// });
 
-socket.on("tweet", function (tweet) {
+socket.on("tweet", function (tweets) {
 
-    document.querySelector("#messages").innerHTML += "<li>" + tweet.author + ":" + tweet.body + " </li>";
-    console.log(tweet)
+    // document.querySelector(".positive").innerHTML = "<h2>Most Positive Tweet</h2>" + '<div class="tweet"> <h3>' + tweets[tweets.length - 1].author + '<h3>' + "<p>" + tweets[tweets.length - 1].body + "</p> </div>";
+    document.querySelector(".positive").innerHTML = `<h2>Most Positive Tweet</h2><div class="tweet"><h3>${tweets[tweets.length - 1].author}</h3><img class="avatar" src="${tweets[tweets.length - 1].avatar}"></img><p>${tweets[tweets.length - 1].body}</p></div>`;
+    document.querySelector(".negative").innerHTML = `<h2>Most Negative Tweet</h2><div class="tweet"><h3>${tweets[0].author}</h3><img class="avatar" src="${tweets[0].avatar}"></img><p>${tweets[0].body}</p></div>`;
+
+    // if (tweets.length < 10) {
+
+    // document.querySelector(".feed").innerHTML = tweets.foreach(tweet => {
+    //     `<div class="tweet"><h3>${tweet.author}<h3><p>${tweets.body}</p></div>`
+    // })
+
+
+    // console.log(tweets)
 })
+
+socket.on("autoFeed", function (tweets) {
+    console.log(tweets)
+    tweets.forEach(tweet => {
+        document.querySelector(".feed").innerHTML += `<div class="tweet"><h3>${tweet.author}</h3><img class="avatar" src="${tweet.avatar}"></img><p>${tweet.body}</p></div>`
+    });
+
+})
+
