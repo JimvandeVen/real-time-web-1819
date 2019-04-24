a984235a0f82c5af5741b3dbf84472c31e6cd27e
JimvandeVen <jimvandeven_1@hotmail.com>
2019-04-24 14:34:55 +0200

    fixed the bugs where everything gets done as many times as there are sockets. much less buggy now

--- index.js
+++ index.js
@@ -27,11 +27,94 @@ function index(req, res) {
     res.render("pages/index");
 };
 
+let proTweets = []
+let conTweets = []
+let connectionCount = 0
+
 let stream = T.stream('statuses/filter', { track: '#brexit', language: 'en', tweet_mode: "extended" })
+
+stream.on('tweet', function (stream) {
+    // console.log("tweet");
+    streamHandler(stream)
+})
+
+let proCount = 0
+let conCount = 0
+
+function streamHandler(stream) {
+    const pattern = "RT @";
+    if (stream.in_reply_to_status_id === null && stream.text.indexOf(pattern) < 0 && stream.quoted_status_id === undefined) {
+        checkFunc(stream)
+    }
+}
+
+function checkFunc(stream) {
+    if (stream.extended_tweet) {
+        extendedTweet(stream)
+    } else {
+        tweet(stream)
+    }
+}
+
+async function extendedTweet(stream) {
+
+    function crObj(stream) {
+        let tweet = {
+            twid: stream.id,
+            active: false,
+            author: stream.user.name,
+            avatar: stream.user.profile_image_url,
+            body: stream.extended_tweet.full_text,
+            date: stream.created_at,
+            screenname: stream.user.screen_name,
+            sentiment: sentiment.analyze(stream.extended_tweet.full_text),
+            likes: 0
+        };
+        return tweet
+    }
+    await tweetSorter(crObj(stream))
+
+}
+
+async function tweet(stream) {
+
+    function crObj(stream) {
+        let tweet = {
+            twid: stream.id,
+            active: false,
+            author: stream.user.name,
+            avatar: stream.user.profile_image_url,
+            body: stream.text,
+            date: stream.created_at,
+            screenname: stream.user.screen_name,
+            sentiment: sentiment.analyze(stream.text),
+            likes: 0
+        };
+        return tweet
+    }
+    await tweetSorter(crObj(stream))
+
+}
+
+function tweetSorter(tweet) {
+    if (tweet.sentiment.score <= 0) {
+        conTweets.push(tweet)
+        io.to('proBrexit').emit('autoFeed', tweet);
+        proCount++
+        console.log("contweets")
+    } else {
+        proTweets.push(tweet)
+        io.to('conBrexit').emit('autoFeed', tweet);
+        conCount++
+        console.log("protweets")
+
+    }
+}
 
 
 io.on("connection", function (socket) {
-    let connectionCount = 0
+
+
 
     socket.on('joinPro', function () { //Janno vragen wat ie hier nou precies bedoelde!!!!!!!!!
         socket.join('proBrexit');
@@ -49,90 +132,17 @@ io.on("connection", function (socket) {
 
 
     if (connectionCount >= 1) {
-        stream.on('tweet', function (stream) {
-            console.log("tweet");
-            streamHandler(stream)
-        })
-    }
-
-
-
-
-    let proTweets = []
-    let conTweets = []
-    let proCount = 0
-    let conCount = 0
-
-
-    function streamHandler(stream) {
-        const pattern = "RT @";
-        if (stream.in_reply_to_status_id === null && stream.text.indexOf(pattern) < 0 && stream.quoted_status_id === undefined) {
-            checkFunc(stream)
-        }
-    }
-
-    function checkFunc(stream) {
-        if (stream.extended_tweet) {
-            extendedTweet(stream)
-        } else {
-            tweet(stream)
-        }
-    }
-
-    async function extendedTweet(stream) {
-
-        function crObj(stream) {
-            let tweet = {
-                twid: stream.id,
-                active: false,
-                author: stream.user.name,
-                avatar: stream.user.profile_image_url,
-                body: stream.extended_tweet.full_text,
-                date: stream.created_at,
-                screenname: stream.user.screen_name,
-                sentiment: sentiment.analyze(stream.extended_tweet.full_text),
-                likes: 0
-            };
-            return tweet
-        }
-        await tweetSorter(crObj(stream))
 
     }
 
-    async function tweet(stream) {
 
-        function crObj(stream) {
-            let tweet = {
-                twid: stream.id,
-                active: false,
-                author: stream.user.name,
-                avatar: stream.user.profile_image_url,
-                body: stream.text,
-                date: stream.created_at,
-                screenname: stream.user.screen_name,
-                sentiment: sentiment.analyze(stream.text),
-                likes: 0
-            };
-            return tweet
-        }
-        await tweetSorter(crObj(stream))
 
-    }
 
-    function tweetSorter(tweet) {
-        if (tweet.sentiment.score <= 0) {
-            conTweets.push(tweet)
-            io.to('proBrexit').emit('autoFeed', tweet);
-            proCount++
-            console.log("contweets")
-        } else {
-            proTweets.push(tweet)
-            io.to('conBrexit').emit('autoFeed', tweet);
-            conCount++
-            console.log("protweets")
 
-        }
-    }
+
+
+
+
 
 
     socket.on("likeHandler", function (like) {
@@ -144,7 +154,7 @@ io.on("connection", function (socket) {
                     } else {
                         conTweet.likes++
                     }
-                    console.log(conTweet)
+                    console.log("conTweetLike")
                     io.to('conBrexit').emit('likes', conTweets.sort(compare));
 
                 }
@@ -158,7 +168,7 @@ io.on("connection", function (socket) {
                     } else {
                         proTweet.likes++
                     }
-                    console.log(proTweet)
+                    console.log("proTweetLike")
                     io.to('proBrexit').emit('likes', proTweets.sort(compare));
                 }
             })
